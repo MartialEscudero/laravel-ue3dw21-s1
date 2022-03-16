584024424796b0736467f4db2f0b75b6e8ed90f0
MartialEscudero <martial.escudero@gmail.com>
2022-03-16 14:02:05 +0100

    first commit

---
+++ app/Http/Controllers/ControleurMembres.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+// Obligatoire pour avoir accès au modèle
+use App\Models\Membre;
+
+class ControleurMembres extends Controller
+  {
+  public function index() {
+    $les_membres = Membre::all();
+    return view('pages_site/membres', compact('les_membres'));
+  }
+}
